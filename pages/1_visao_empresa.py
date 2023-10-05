56bbd520e3603fb1f32725892d9a86679e3757ec
Edinan Marinho <61295729+EdinanMarinho@users.noreply.github.com>
2023-10-05 09:20:32 -0300

    Update 1_visao_empresa.py
    import datetime

--- pages/1_visao_empresa.py
+++ pages/1_visao_empresa.py
@@ -3,6 +3,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 import plotly.express as px
 from haversine import haversine
+from datetime import datetime
 
 # Bibliotecas necessarias
 import pandas as pd
@@ -165,9 +166,9 @@ st.sidebar.markdown( '## Selecione uma data limite')
 
 date_slider = st.sidebar.slider( 
     'Até qual valor ?',
-    value=pd.datetime( 2022, 4, 1 ),
-    min_value=pd.datetime(2022, 2, 11 ),
-    max_value=pd.datetime( 2022, 4, 6 ),
+    value=datetime( 2022, 4, 1 ),
+    min_value=datetime(2022, 2, 11 ),
+    max_value=datetime( 2022, 4, 6 ),
     format='DD-MM-YYY' )
 
 st.sidebar.markdown( """___""")
